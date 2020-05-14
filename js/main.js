7b1cb8c913fc391cc661e03859a5a325aed5611c
Timalk16 <tima160604@mail.ru>
2020-05-14 12:01:58 +0300

    refact: rewrite js script to jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event){
     const modal = document.querySelector('.modal')
     const modalBtn = document.querySelectorAll('[data-toggle=modal]')
@@ -10,7 +11,39 @@ document.addEventListener("DOMContentLoaded", function(event){
     modalBtn.forEach(element => {
         element.addEventListener('click', swichModal);
     });
+    // закрытие модального окна нажатием на esc
+    document.addEventListener('keydown', function(element) {
+        let keyCode = element.keyCode;
+        if (keyCode === 27) {
+            modal.classList.remove('modal--visible');
+        
+        }
+    });
 
 
+    // закрытие модального окна при клике вне него
+    document.addEventListener("click", function(e) {
+        if (e.target.className === "modal-click") {
+         modal.classList.remove('modal--visible');
+        }
+      });
+    
     closeBtn.addEventListener('click', swichModal);
 }); 
+*/
+
+
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+
+    
+});
