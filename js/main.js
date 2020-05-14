4e7a1b3e17bff93a5f7288508d353b6883b3ae8a
Timalk16 <tima160604@mail.ru>
2020-05-14 13:37:32 +0300

    feat add jquery scroll to layout

--- js/main.js
+++ js/main.js
@@ -45,5 +45,12 @@ $(document).ready(function () {
         modal.toggleClass('modal--visible');
     });
 
+    $(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        });
+});
     
 });
