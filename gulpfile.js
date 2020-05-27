cd75fc8ae268992ccd68c37069c49a482e3a03a0
Timalk16 <tima160604@mail.ru>
2020-05-27 12:36:10 +0300

    feat: optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
 const browserSync = require('browser-sync').create();
 
 
@@ -35,4 +39,66 @@ const browserSync = require('browser-sync').create();
         .pipe(browserSync.stream());
 };
 
+//minify css
+function buildCSS(done) {
+    src('css/**.css').pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(dest('dist/css'))
+    done();
+}
+//minify js
+function buildJS(done) {
+    src(['js/**.js', '!js/**.min.js'])
+    .pipe(minify())
+    .pipe(dest('dist/js'));
+    src('js/**.min.js').pipe(dest('dist/js'));
+    done();
+}
+
+
+//minify html
+function html(done) {
+    src('**.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist/'))
+    done();
+    
+}
+
+
+
+//minify php
+function php(done) {
+    src('**.php')
+        .pipe(dest('dist/'))
+    src('phpmailer/**/**')
+        .pipe(dest('dist/phpmailer'))
+    done();
+    
+}
+//fonts
+function fonts(done) {
+    src('fonts/**/**')
+    .pipe(dest('dist/fonts'))
+    done();
+    
+}
+
+//img
+function imgmin(done) {
+    src('img/**/**/*.{png,jpg,jpeg}')
+    .pipe(tinypng({
+        key: '8VnMgzlSjZCyymgDxDchczx6xkgqTvmf',
+        sigFile: 'images/.tinypng-sigs',
+        log: true
+    }))
+        .pipe(dest('dist/img/'))
+    src('img/**/**.svg')
+        .pipe(dest('dist/img/'));
+    done();
+
+}
+
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, fonts, imgmin);
+
--- js/main.js
+++ js/main.js
@@ -5,25 +5,39 @@ $(document).ready(function () {
         closeBtn = $('.modal__close');
 
     modalBtn.on('click', function () {
+        $('.modal__form')[0].reset();
+        $('em.invalid').remove();
+        $('.modal__form').find(".invalid").removeClass("invalid");
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.removeClass('modal--visible');
+        $('modal__form')[0].reset();
+        $('em.invalid').remove();
+        $('modal__form').find(".invalid").removeClass("invalid");
         modal.toggleClass('modal--visible');
     });
     // закрытие модального окна нажатием на esc
     $(this).keydown(function(eventObject){
-        if (eventObject.which == 27)
-        modal.toggleClass('modal--visible');
+        if (eventObject.which == 27) {
+            $('.modal__form')[0].reset();
+            $('em.invalid').remove();
+            $('.modal__form').find(".invalid").removeClass("invalid");
+            modal.toggleClass('modal--visible')
+        }
     });
 
     // закрытие модального окна при клике вне него
     $(document).click(function (e) {
         if ($(e.target).is('.modal')) {
+            $('.modal__form')[0].reset();
+            $('em.invalid').remove();
+            $('.modal__form').find(".invalid").removeClass("invalid");
             modal.toggleClass('modal--visible');
         }
     });
 
     
-    closeBtn.on('click', function () {
-        modal.toggleClass('modal--visible');
-    });
     //медленный скролл
     $(function(){
         $(window).scroll(function(){
@@ -233,6 +247,50 @@ $(document).ready(function () {
 
     //маска для телефона
     $('[type=tel]').mask('+7(000) 000-00-00');
+    
+
+    //map
+      // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  setTimeout(function(){
+	var elem = document.createElement('script');
+	elem.type = 'text/javascript';
+	elem.src = '//api-maps.yandex.ru/2.1/?apikey=c5d2f113-43c7-49b9-a946-6afe16d0b56b&lang=ru_RU';
+	document.getElementsByTagName('body')[0].appendChild(elem);
+}, 2000);
+ ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [47.244729, 39.723187],
+            zoom: 17
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+        myMap.behaviors.disable('scrollZoom');
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Наш офис'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/pin.png',
+            // Размеры метки.
+            iconImageSize: [35, 35],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
 
 
     
@@ -241,6 +299,7 @@ $(document).ready(function () {
 
 
     new WOW().init();
+    $('form').find('input').val("");
 
     });
 
